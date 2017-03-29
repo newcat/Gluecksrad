34ce33a42b18c60f363525e92226b605e516feb2
newcat <kungkung@hotmail.de>
2017-03-29 14:25:33 +0200

    Added sentence file

---
+++ Datamodel/Datamodel/Sentences.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#define SENTENCE_COUNT 20
+
+const char* all_sentences[] = {
+	"*************ABWARTEN UND *TEE TRINKEN**************",
+	"*************ALLES GUTE ****KOMMT VON******OBEN*****",
+	"ALLES HAT EIN ENDE NUR DIE *WURST HAT *****ZWEI*****",
+	"*************ALTE LIEBE **ROSTET NICHT**************",
+	"***************ALTER VOR****SCHOENHEIT**************",
+	"***************ALLES NEU ***MACHT DER ******MAI*****",
+	"****ALTER******SCHUETZT ***VOR TORHEIT ****NICHT****",
+	"**AM ABEND ****WIRD DER ******FAULE******FLEISSIG***",
+	"**HUNDE DIE ****BELLEN, *****BEISSEN ******NICHT****",
+	"****************REDBULL *****VERLEIT *****FLÜGEL****",
+	"**************NICHTS IST **UNMOEGLICH **- TOYOTA****",
+	"**WOHNST DU ***NOCH ODER ***LEBST DU ******SCHON****",
+	"**************SCHREI VOR*****GLUECK*****************",
+	"***************DES VODKAS**REINE SEELE**************",
+	"**************DU WILLST *****ES. DU ****KRIEGST ES**",
+	"**************EINMAL HIN, *ALLLES DRIN**************",
+	"*************UHU PATAFIX! *FIXERT FIX!**************",
+	"****************UNTERM ******STRICH ****ZAEHLE ICH**",
+	"**************QUADRATISCH. PRAKTISCH. ******GUT*****",
+	"***************GESCHMACK ******IST ********KING*****"
+};
